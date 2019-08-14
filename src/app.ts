19ce11f048b6e7f8343bee492f06386d703ffc3a
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-14 15:05:00 -0500

    Exploring this with apply, call, and bind

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,6 @@
 
 // `this` keyword changes based on how we invoke the function.
 
-// Function demonstrating this context.
-// In this case it is the window object in the browser.
-function myFunction() {
-  console.log('Function', this);
-}
-
-// Invoke function.
-// The window is invoking this function.
-myFunction();
-
 // Creating an object literal.
 const myObject = {
   myMethod() {
@@ -24,15 +14,36 @@ const myObject = {
 // Because myObject is invoking the myMethod, `this` refers to the myObject.
 myObject.myMethod();
 
-// Classes
-class myClass {
-    myMethod() {
-        console.log('Class' this);
-    }
+// Function demonstrating this context.
+// In this case it is the window object in the browser.
+function myFunction(...text: string[]) {
+  console.log('Function', this);
 }
 
-// Create Instance of the class.
-const myInstance = new myClass();
-// Invoke class example.
-// Because myInstance is invoking myClass, `this` refers to the class.
-myInstance.myMethod();
+// Invoke function.
+// The window is invoking this function.
+myFunction('ABC', 'DEF');
+//   Window is invoking the function, however the call is changing the value to whatever
+//  we supply as the first argument.
+myFunction.call(myObject, 'abc', 'def');
+// We are supplying an array instead of the argument.
+myFunction.call([]);
+
+// .call and .apply invoke the function.
+// .call is comma separated. .apply is passed an array.
+// These are used to potentially change the this context when you need to pass
+// an argument down to the code.
+
+// .bind does not invoke the function. It returns a bound function with a few differences.
+// Context has already been changed and can be used later or multiple times.
+// Arguments can be applied ahead of time, but only likely to be done if you have
+// a single argument that you expect to reuse.
+const bindFunction = myFunction.bind(myObject, 'AbC', 'dEf');
+
+// Invoke the function.
+bindFunction();
+
+// Arguments can be supplied later as well.
+// Changing the arguments with each function call is more practical.
+const bindFunction2 = myFunction.bind(myObject);
+bindFunction2('aBc', 'DeF');
