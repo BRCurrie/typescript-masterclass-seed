bf3ac0a41f2f43a5d1e2fde0056c80712cb41be9
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-15 12:22:03 -0500

    keyof Index Type Queries

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// Typescript infers type here.
+// We would use this in a case where we are building a library or an application.
+
+// We have an anonymous person.
+// We want to take name and age and bind it to a union type.
 const person = {
   name: 'Todd',
   age: 27
 };
 
-// If for some reason we cannot access type definitions:
-// We want to access the anonymous type.
-// We essentially query the type of person. We now have a reference to the inferred
-// type above.
-// This would be done if the type would be used in multiple places.
+// Create a new type.
 type Person = typeof person;
+// We make a typesafe lookup by using the keyof query.
+// name | age ; string literals.
+type PersonKeys = keyof Person;
 
-// We can now use the type definition from Person.
+// Give it a value of Person and pass in PersonKeys.
+// This is the creation of the union type.
+type PersonTypes = Person[PersonKeys];
+
+// Bind to the new type.
 const anotherPerson: Person = {
   name: 'John',
   age: 26
-};
-
-// Type query inline. For single use type checking.
-const inlinePerson: typeof person = {
-  name: 'Ben',
-  age: 30
 };
