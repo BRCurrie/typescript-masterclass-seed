ba2e142279315c76b538935bb707e3da7c1eca67
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-08-14 21:16:53 -0500

    Typing this and noImplicitThis

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,23 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-class MyClass {
-  myMethod() {
-    const foo = 123;
-    const that = this;
-    // This keyword references back to the class.
-    console.log('Initial Function', this);
-    // Added after first instance call.
-    // When we create a new function, we create a new scope.
-    // The foo is a new scope but is also part of the scope chain.
-    // This is part of the lexical scope.
-    setTimeout(function() {
-      console.log('foo', foo);
-      // Each new function gets its own this keyword.
-      // This keyword would reference to the window.
-      console.log('First SetTimeout', this);
-    }, 0);
-    // With this function we setup that = this above. The scope of this refers
-    // back to the MyClass object. This is an old way of doing this.
-    setTimeout(function() {
-      console.log('That', that);
-    }, 0);
-    // Arrow functions do not bind a this value.
-    // const that = this is no longer necessary because the arrow function never changed
-    // the scope.
-    setTimeout(() => {
-      console.log('Arrow', this);
-    }, 0);
-  }
-  // This method creates separate scope from foo in the other method.
-  // They do not interfere with each other.
-  //   We do not call this function at any point in this demonstration.
-  foo() {
-    const foo = 456;
-  }
+// Added <a> tag on index.html for this example.
+// Added noImplicitThis to tsconfig.json.
+
+// Now we are accessing the .click class on the <a> tag.
+const elem = document.querySelector('.click');
+
+// We have type definition for dom objects already.
+// To type the this keyword we have to add what looks like an argument.
+// It is NOT an argument, and will not change the arguments.
+// event is still the first argument.
+function handleClick(this: HTMLAnchorElement, event: Event) {
+  // Prevent default behavior of the link.
+  event.preventDefault();
+  // We have added a type to `this` and now we can use autocompletion.
+  console.log(this);
+  console.log(this.className);
 }
 
-const myInstance = new MyClass();
-myInstance.myMethod();
+// The element invokes the function, so `this` refers to it.
+elem.addEventListener('click', handleClick, false);
