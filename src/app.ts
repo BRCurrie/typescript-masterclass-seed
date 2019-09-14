9b10968ccd7f0f752c84a04fb1e8de1a3b8249e7
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-09-14 18:23:34 -0500

    typeof and Type Guards

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,62 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// Create dictionary type as object which holds key and string, and have any value.
-// Has the base of an empty object.
-// let dictionary: { [key: string]: any} = {};
+// Typeguard is a way that we can get type information after making a check
+// inside a conditional.
+// When we use a typeguard we get type information iside that conditional block.
 
-let dictionary: Record<string, TrackStates> = {};
+// Typeof operator exists in JS.
 
-// if TrackStates interface was not available to us we could use typeof.
-let dictionaryItem: Record<string, typeof item> = {};
-
-// Interface for a music application. Holds the current and next tracks.
-interface TrackStates {
-  current: string;
-  next: string;
+// demonstration
+function foo(bar: string | number) {
+  if (typeof bar === 'string') {
+    // string value.
+    // bar. gives string options.
+    return bar.toUpperCase();
+  }
+  // until something is done in the if statement above, this can still be of type string or number.
+  // once something is handle in the case of the string value aboue then bar will now be
+  // inferred as a number type.
+  bar;
+  // inferred as only a number now.
+  // we could use another if statement as a safety check instead.
+  // if (typeof bar === 'number') {
+  // return bar.toFixed(2);
+  // }
 }
 
-// const item: TrackStates = {
-// current: 'js02js9',
-// next: '8nlksjsk'
-// };
+// We need to create a typeguard for duration if it is a string or a number.
+// We might be dealing with different number formats.
 
-// keyof returns us a union type based off of the interface. We then grab their properties and their
-// value will be a string.
-// If we want to access multiple properties we need to access it this way and set those properties.
-const item: Record<keyof TrackStates, string> = {
-  current: 'js02js9',
-  next: '8nlksjsk'
-};
+class Song {
+  constructor(public title: string, public duration: string | number) {}
+}
 
-// JS indexing: numbers are coerced to String. '0'
-// This would be if we just want to access one single object.
-dictionary[0] = item;
+function getSongDuration(item: Song) {
+  if (typeof item.duration === 'string') {
+    return item.duration;
+  }
+  // Since its not a string it must be a number.
+  // return item.duration;
+
+  // instead of returning we will create a few constants.
+  // This will calculate from milliseconds to a more legible duration.
+  const { duration } = item;
+  const minutes = Math.floor(duration / 60000);
+  const seconds = (duration / 1000) % 60;
+  return `${minutes}:${seconds}`;
+}
+
+const songDurationFromString = getSongDuration(
+  new Song('Wonderful Wonderful', '05:31')
+);
+console.log(songDurationFromString);
+
+const songDurationFromMS = getSongDuration(
+  new Song('Wonderful Wonderful', 330000)
+);
+console.log(songDurationFromMS);
+
+// After compiling and running we Expect 05:31 to be returned from the String function.
+// Followed by the calculated duration from the number as 5:30.
+// We would need to add logic to add a leading 0 as needed.
