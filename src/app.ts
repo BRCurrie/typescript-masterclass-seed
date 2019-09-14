e28672e60b089ef4ca4f0781683535649a35dabd
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-09-14 16:39:24 -0500

    Record Mapped Type

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,34 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-interface Person {
-  name: string;
-  age: number;
-  address: {};
+// Create dictionary type as object which holds key and string, and have any value.
+// Has the base of an empty object.
+// let dictionary: { [key: string]: any} = {};
+
+let dictionary: Record<string, TrackStates> = {};
+
+// if TrackStates interface was not available to us we could use typeof.
+let dictionaryItem: Record<string, typeof item> = {};
+
+// Interface for a music application. Holds the current and next tracks.
+interface TrackStates {
+  current: string;
+  next: string;
 }
 
-// For every key:
-// It must exist in the object.
-// We are only going to supply the result back for properties that exist.
-type myPick<T, K extends keyof T> = {
-  [P in K]: T[P];
-};
-// K is a subtype of our union type T.
-// T is the first argument supplied, the interface of Person.
-// We can now select the properties and obtain only these properties.
-// This saves us from having to make the address an optional property.
+// const item: TrackStates = {
+// current: 'js02js9',
+// next: '8nlksjsk'
+// };
 
-const person: myPick<Person, 'name' | 'age'> = {
-  name: 'Todd',
-  age: 27
+// keyof returns us a union type based off of the interface. We then grab their properties and their
+// value will be a string.
+// If we want to access multiple properties we need to access it this way and set those properties.
+const item: Record<keyof TrackStates, string> = {
+  current: 'js02js9',
+  next: '8nlksjsk'
 };
+
+// JS indexing: numbers are coerced to String. '0'
+// This would be if we just want to access one single object.
+dictionary[0] = item;
