9982dffd625b9937cbff78d76116a8197838c511
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-09-14 23:30:09 -0500

    User Defined Type Guards

--- src/app.ts
+++ src/app.ts
@@ -5,68 +5,44 @@
 // inside a conditional.
 // When we use a typeguard we get type information iside that conditional block.
 
-// Demo of instaceof
-// This is sugar syntax essentially for the function below.
-class Foo {
-  bar() {}
-}
-// ES5 compiled from class and method above.
-// function Foo2() {}
-// Foo2.prototype.bar = function () {};
-
-const bar = new Foo();
-
-// We test whether prototype property of constructor exists in another object.
-// the value of bar is an instance of Foo. Bar is an instance of Foo.
-// Foo.prototype is not creating a new instance we are just checking for one.
-console.log(Object.getPrototypeOf(bar) === Foo.prototype);
-// compile then run node and we log true;
-
-// We can use the instance of keyword to check as well.
-console.log(bar instanceof Foo);
-// console true
+// In a scenario where you would want to break something down into a reusable function
+// you lose the type inference if it is not declared.
 
 class Song {
   constructor(public title: string, public duration: number) {}
 }
 
-// The playlist property will have an array of songs
 class Playlist {
   constructor(public name: string, public songs: Song[]) {}
 }
 
-// Wether we pass it a song or playlist we want the name.
-// We are definitely returning a string from this function because title and name properties
-// are both of type string.
+// Does not work at compile time. currently returns boolean of existance of instance.
+// item is Song is added here. When it evaluates to true we are returning Type information for Song.
+function isSong(item: any): item is Song {
+  return item instanceof Song;
+  // Can only use the is syntax when testing something like a boolean.
+  // return {}; would not work.
+}
+
 function getItemName(item: Song | Playlist) {
-  // We might manually check if item has a title property with a type assertion as a Song
-  // if ((item as Song).title) {
-  //   // assumed it was a song
-  //   // return item. does not work here we need to add typing again.
-  //   return (item as Song).title;
+  // instanceof typeguard example
+  // if (item instanceof Song) {
+  //   return item.title;
   // }
-  // Kind of long winded. We use instance of instead...
-  if (item instanceof Song) {
-    // TS will now infer that it is a Song and the properties there will be available.
+  // return item.name;
+
+  if (isSong(item)) {
     return item.title;
   }
-  // Manual check requires `as` here too.
-  // // return (item as Playlist).name;
-  // But with instanceof TS will now infer we must have a playlist
-  // playlist properties will now be available.
   return item.name;
 }
 
-// We invoke our function and pass in a new song title and  duration.
 const songName = getItemName(new Song('Wonderful Wonderful', 300000));
-// Console long the value of the songName.
-console.log('Song name:', songName);
-// expect Wonderful Wonderful
 
-// We invoke our function and pass in a playlist with a playlist name and array of songs.
+console.log('Song name:', songName);
+
 const playlistName = getItemName(
   new Playlist('The Best Songs', [new Song('The Man', 300000)])
 );
-// Console log the name of the playlist.
+
 console.log('Playlist name:', playlistName);
-// expect The Best Songs
