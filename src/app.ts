126b03ab7e9615207503495d6a8c562740e2eb1f
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-10-17 12:12:49 -0500

    Function Generics

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,47 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// Interfaces vs Classes
-
-// Class is a blueprint where we can create objects that share the same info, properties, and methods.
-// Class can contain implementation details.
-
-// Interface is a group of info that describe an object.
-
-// Neither provide implementation or initialization.
-
-// Both options provide type checking, but only class contains implementation details.
-// This is pretty much the determining factor for which to use.
-
-interface Artist {
-  name: string;
+class Pizza {
+  constructor(private name: string, private price: number) {}
 }
 
-// We implement the interface on the class for type checking.
-class ArtistCreator {
-  constructor(public name: string) {}
+// It is common to create a list and add or retrive items from it.
+
+// We supply a type variable to List so we can pass in a type variable.
+class List<T> {
+  // private list: any[];
+  // We can now pass in the variable array type without specifying any.
+  private list: T[];
+
+  // addItem to a list without returning anything.
+  // This is a common pattern that we would use.
+  addItem(item: T): void {
+    this.list.push(item);
+  }
+
+  // We expect this to return an array.
+  getList(): T[] {
+    return this.list;
+  }
 }
 
-function artistFactory({ name }: Artist) {
-  // We would expect a returned id from say a backend based on the name argument provided.
-  // The name is strictly type checked, and that is all.
-  return { id: 101, name };
+// We can add the Pizza array typing to the list and the information will be passed down.
+const list = new List<Pizza>();
+
+list.addItem(new Pizza('Pepperoni', 15));
+// We might accidentally add another class. We can automatically add to this list and it is
+// quite error prone.
+// list.addItem(new Coupon());
+
+const pizzas = list.getList();
+
+// Another example using our generic funtion.
+
+class Coupon {
+  constructor(private name: string) {}
 }
 
-function ArtistFactoryClass({ name }: ArtistCreator) {
-  // provides implementation details.
-  // This creates a new instance and can only be done with classes.
-  return new ArtistCreator(name);
-}
+// We can define the value at the point it is called and use it throughout the application.
+const anotherList = new List<Coupon>();
 
-// When we call the functions we need to supply the name argument.
-ArtistFactoryClass({ name: 'Todd' });
+anotherList.addItem(new Coupon('PIZZA25'));
