8db85d0b3b0eeeb514333c2bbfb13d7a6bcd0a39
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-10-18 09:18:12 -0500

    @types/lodash installed

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,5 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// We can use string values instead of numeric enums.
-// We lose reverse lookup however.
-// By adding const we can make this an inline member. It will reduce the JS file.
-// See app.js after compiling and try with and without const.
-const enum Sizes {
-  Small = 'small',
-  Medium = 'medium',
-  Large = 'large'
-  // We could use a number as well.
-  // ExtraLarge = 3,
-}
-
-// We set the default as Small.
-let selected: Sizes = Sizes.Small;
-
-// We can use an enum as a type to a function argument.
-function updateSize(size: Sizes): void {
-  selected = size;
-}
-
-// Update our selected size to Large.
-updateSize(Sizes.Large);
-console.log(selected);
+import * as lodash from 'lodash';
+// Use "lodash"
