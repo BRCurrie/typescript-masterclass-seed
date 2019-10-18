f3195503f5a66718453faad0b094b57936067fad
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-10-18 00:17:44 -0500

    Numeric Enums and Reverse Mappings

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,26 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// Function Overloads
-// Utility functions. Pure functions that typically return a different data structure or
-// data set.
+// Enum is a datatype. Parses out to JS with a set of numeric values.
+// See app.js
 
-// The function overload does not compile into JS differently. It is only there for
-// typechecking purposes.
-
-// This is a type declaration.
-// This code would not get compiled down to JS.
-function reverse(str: string): string;
-// We use a generic type to pass the type checking along to the function calls below.
-function reverse<T>(arr: T[]): T[];
-
-// We declare to TS that we will feed the function different parameters that will give it
-// a different result.
-// This is the complete implementation function.
-function reverse<T>(stringOrArray: string | any[]): string | any[] {
-  if (typeof stringOrArray === 'string') {
-    return stringOrArray
-      .split('')
-      .reverse()
-      .join('');
-  }
-  // We slice the array to create a copy then reverse the new array.
-  return stringOrArray.slice().reverse();
+enum Sizes {
+  Small,
+  Medium,
+  Large
+}
+// We cannot have this default to 0, so the array index of 3 must be supplied.
+enum Sizes {
+  ExtraLarge = 3
 }
 
-reverse('Pepperoni');
-reverse(['bacon', 'pepperoni', 'chili', 'mushrooms']);
+// Expect numeric value in the array index. 1
+console.log(Sizes.Medium);
+
+// We can use reverse mapping like this, and it will return the string value.
+console.log(Sizes[Sizes.Large]);
+
+const selectedSize = 2;
+// We can use the numeric value to return the string value.
+// Expect Large to be logged.
+console.log(Sizes[selectedSize]);
