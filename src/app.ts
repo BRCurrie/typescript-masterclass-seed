98aa84fa6d2f4f9c58ec1daab580f5f64486388d
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-10-18 00:25:11 -0500

    String Enums and Inlining Members

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,26 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-// Enum is a datatype. Parses out to JS with a set of numeric values.
-// See app.js
-
-enum Sizes {
-  Small,
-  Medium,
-  Large
+// We can use string values instead of numeric enums.
+// We lose reverse lookup however.
+// By adding const we can make this an inline member. It will reduce the JS file.
+// See app.js after compiling and try with and without const.
+const enum Sizes {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large'
+  // We could use a number as well.
+  // ExtraLarge = 3,
 }
-// We cannot have this default to 0, so the array index of 3 must be supplied.
-enum Sizes {
-  ExtraLarge = 3
+
+// We set the default as Small.
+let selected: Sizes = Sizes.Small;
+
+// We can use an enum as a type to a function argument.
+function updateSize(size: Sizes): void {
+  selected = size;
 }
 
-// Expect numeric value in the array index. 1
-console.log(Sizes.Medium);
-
-// We can use reverse mapping like this, and it will return the string value.
-console.log(Sizes[Sizes.Large]);
-
-const selectedSize = 2;
-// We can use the numeric value to return the string value.
-// Expect Large to be logged.
-console.log(Sizes[selectedSize]);
+// Update our selected size to Large.
+updateSize(Sizes.Large);
+console.log(selected);
