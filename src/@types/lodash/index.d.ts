8f7dc885f286cc351da3f3067066adb44d3bc185
Ben Currie <benjaminrussellcurrie@gmail.com>
2019-10-21 12:42:52 -0500

    Writing Declaration Files

---
+++ src/@types/lodash/index.d.ts
@@ -0,0 +1,5 @@
+// d stands for declaration. index.d.ts
+
+declare module 'lodash' {
+  export function chunk(collection: any, size?: number): any[][];
+}
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 // `npm start` or `yarn start` to see in browser.
 // `tsc` then `node dist/app.js` to see in terminal.
 
-import * as lodash from 'lodash';
+import * as _ from 'lodash';
 // Use "lodash"
+
+_.chunk([1, 2, 3, 4], 2); // [[1,2], [3,4]]
